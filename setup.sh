19af91e18f914911f721c93d4653d4fa29970e18
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2018-03-22 09:44:42 -0500

    setup script generic

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
-export UBXSecAnaRoot=/Users/deltutto/RealWork/CCInclusiveEventSelection/ubxsecana/Root/
-export UBXSecAnaFluxFile=/Users/deltutto/RealWork/CCInclusiveEventSelection/ubxsecana/Flux/numode_bnb_470m_r200.root
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/deltutto/RealWork/CCInclusiveEventSelection/ubxsecana_build/Libraries/Source/
+#!/bin/bash
+
+mkdir -p Build/
+mkdir -p Files/
+
+export UBXSecAnaRoot=`pwd`/Root/
+export UBXSecAnaFluxFile=`pwd`/Flux/numode_bnb_470m_r200.root
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:`pwd`/Build/Libraries/Source/
