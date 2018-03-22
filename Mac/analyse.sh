661ab267e218e337794d2269b40dc3179715cf9d
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2018-03-22 09:47:47 -0500

    modified scripts

--- Mac/analyse.sh
+++ Mac/analyse.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 Analyse -m ubxsecana_output_bnbcosmic_mcc8.7_test1.root -c ubxsecana_output_intimecosmic_mcc8.3_v2.0.2.root -b ubxsecana_output_bnbon_mcc8.7_test1.root -e ubxsecana_output_extbnb_mcc8.7_test1.root -p 4.106e+19 -n 9161991 -g 48845676
+
+
 #Analyse -m ubxsecana_output_bnbcosmic_mcc8.6_stopmu_test29.root -c ubxsecana_output_intimecosmic_mcc8.3_v2.0.2.root -b ubxsecana_output_bnbon_mcc8.6_stopmu_test29.root -e ubxsecana_output_extbnb_mcc8.6_stopmu_test29.root -p 4.54e+19 -n 10141372 -g 14386908
 #Analyse -m ubxsecana_output_bnbcosmic_mcc8.6_stopmu_test23.root -c ubxsecana_output_intimecosmic_mcc8.3_v2.0.2.root -b ubxsecana_output_bnbon_mcc8.6_stopmu_test23.root -e ubxsecana_output_extbnb_mcc8.6_stopmu_test23.root -p 4.799e+19  # bnbon: 10600435, extbnb: 14261533, scale for extbnb = 10600435/14261533 = 0.7432886072
 #Analyse -m ubxsecana_output_bnbcosmic_mcc8.6_stopmu_test22.root -c ubxsecana_output_intimecosmic_mcc8.3_v2.0.2.root -b ubxsecana_output_bnbon_mcc8.6_stopmu_test22.root -e ubxsecana_output_extbnb_mcc8.6_stopmu_test22.root -p 4.834e+19  # bnbon: 10677340, extbnb: 13451912, scale for extbnb = 10677340/13451912 = 0.7937414399
